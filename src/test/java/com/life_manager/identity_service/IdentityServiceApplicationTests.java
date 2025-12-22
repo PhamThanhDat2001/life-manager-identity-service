af70b215abe4e8458d3ef76226b8878cf1387bf8
datpt <phamthanhdat186@gmail.com>
2025-12-22 22:22:38 +0700

    init

---
+++ src/test/java/com/life_manager/identity_service/IdentityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.life_manager.identity_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
