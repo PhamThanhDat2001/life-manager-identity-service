8f3460706bd1179c7df9e28cafd6d62caf13eecb
datpt <phamthanhdat186@gmail.com>
2025-12-25 23:11:06 +0700

    integration flyway

---
+++ src/main/java/com/life_manager/identity_service/auth/infrastructure/UserEntity.java
@@ -0,0 +1,26 @@
+package com.life_manager.identity_service.auth.infrastructure;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private LocalDate dob;
+
+}
