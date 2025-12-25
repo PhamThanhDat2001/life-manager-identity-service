e94af4efd99ca45fab88eb547d66a96f0487da71
datpt <phamthanhdat186@gmail.com>
2025-12-26 00:12:08 +0700

    wip

--- src/main/java/com/life_manager/identity_service/auth/infrastructure/UserJpaRepository.java
+++ src/main/java/com/life_manager/identity_service/auth/infrastructure/UserJpaRepository.java
@@ -3,4 +3,5 @@ package com.life_manager.identity_service.auth.infrastructure;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserJpaRepository extends JpaRepository<UserEntity, String> {
+
 }
