fdd9808c438e82807a855f09c3ae2e8593b347b5
PhamThanhDat2001 <94340801+PhamThanhDat2001@users.noreply.github.com>
2026-01-06 12:27:30 +0700

    feat/exeption (#5)
    * feat: exeption

--- src/main/java/com/life_manager/identity_service/auth/application/dto/CreateUserRequest.java
+++ src/main/java/com/life_manager/identity_service/auth/application/dto/CreateUserRequest.java
@@ -1,12 +1,16 @@
 package com.life_manager.identity_service.auth.application.dto;
 
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 import java.time.LocalDate;
 
 @Getter
 public class CreateUserRequest {
+    @Size(min = 1, max = 50, message = "USERNAME_INVALID")
     private String username;
+    
+    @Size(min = 8, max = 50, message = "PASSWORD_INVALID")
     private String password;
     private String firstName;
     private String lastName;
--- src/main/java/com/life_manager/identity_service/presentation/UserController.java
+++ src/main/java/com/life_manager/identity_service/presentation/UserController.java
@@ -3,6 +3,8 @@ package com.life_manager.identity_service.presentation;
 import com.life_manager.identity_service.auth.application.UserService;
 import com.life_manager.identity_service.auth.application.dto.CreateUserRequest;
 import com.life_manager.identity_service.auth.infrastructure.UserEntity;
+import com.life_manager.identity_service.presentation.dto.ApiResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,12 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/v1")
+@RequestMapping("/v1/users")
 public class UserController {
     private final UserService userService;
 
     @PostMapping
-    public UserEntity createUser(@RequestBody CreateUserRequest createUserRequest){
-        return userService.createUser(createUserRequest);
+    public ApiResponse<UserEntity> createUser(@RequestBody @Valid CreateUserRequest createUserRequest){
+        ApiResponse<UserEntity> apiResponse = new ApiResponse<>();
+        apiResponse.setResult(userService.createUser(createUserRequest));
+        return apiResponse;
     }
 }
